1fa67ae5ceb6220f425d087c86e2478f383eee59
Prithvi Patil <prithvipatil1357@gmail.com>
2025-04-04 02:14:31 +0530

    made changes to getPoolServerUrl

--- packages/nextjs/utils/getPoolServerUrl.ts
+++ packages/nextjs/utils/getPoolServerUrl.ts
@@ -1,4 +1,12 @@
 import * as chains from "wagmi/chains";
 
-export const getPoolServerUrl = (id: number) =>
-  id === chains.hardhat.id ? "http://localhost:49832/" : "https://backend.multisig.holdings:49832/";
+// Always use the backend URL provided by the environment variable
+export const getPoolServerUrl = () => {
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+  if (!backendUrl) {
+    console.error("CRITICAL ERROR: NEXT_PUBLIC_BACKEND_URL environment variable not set!");
+    // Return an invalid URL to make it clear the configuration is missing
+    return "http://env-var-not-set.invalid/";
+  }
+  return backendUrl;
+};
